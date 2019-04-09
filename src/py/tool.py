c2777d21ff3218785863d6a2e7a48cca5838e406
ivan.heibi <ivanhb.ita@gmail.com>
2019-04-09 13:36:49 +0200

    alloyUI

---
+++ src/py/tool.py
@@ -0,0 +1,21 @@
+class tool(object):
+
+
+    def __init__(self):
+        pass
+
+    #run a func
+    def run(self, id_name, input_data, param = []):
+        res = -1
+        try:
+            res = getattr(self, id_name)(input_data, param)
+            return res
+        except AttributeError:
+            return res
+
+
+    #tools
+    def lda(self, input_data, param = []):
+        return input_data
+
+    
