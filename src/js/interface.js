93f922b4889f8af2829d260c6d9607cead3022f4
ivan.heibi <ivanhb.ita@gmail.com>
2019-04-11 18:04:32 +0200

    overview section

--- src/js/interface.js
+++ src/js/interface.js
@@ -1,12 +1,17 @@
 
 class vwbata {
 
+    GENERAL = {'name': 'My Catarsi Diagram'};
 
     DOMTYPE = {
-      'dataName': {'type':'input_box', 'title': 'Data name'},
-      'toolName': {'type':'input_box', 'title': 'Tool name'},
-      'toolType': {'type': 'dropdown', 'title': 'Tool type','value':[],'label':[]}
+      'graphName': {'type':'input_box', 'title': 'Graph name', 'value':'name'},
+      'dataName': {'type':'input_box', 'title': 'Data name', 'value':'name'},
+      'toolName': {'type':'input_box', 'title': 'Tool name', 'value':'name'},
+      'toolType': {'type': 'dropdown', 'title': 'Tool type', 'value':[],'label':[]}
     }
+
+    OVERVIEW_SECTION = "graphName";
+    INFO_SECTION = { tool: "toolName-toolType", 'data':"dataName"};
 
     info_section_html = "";
     overview_section_html = "";
@@ -27,6 +32,8 @@ class vwbata {
             this.DOMTYPE.toolType.label.push(config_file.tool[k_tool].label);
           }
         }
+
+        this.build_overview();
     }
 
     init_nav() {
@@ -52,59 +59,72 @@ class vwbata {
         return all_tools;
     }
 
-    build_info(node) {
+    build_overview() {
       //first decide what doms should be visualized (defined in DOMTYPE)
-      var dom_key = "";
-      switch (node.type) {
-        case 'tool':
-          dom_key = "toolName-toolType";
-          break;
-        case 'data':
-          dom_key = "dataName";
-          break;
-      }
+      var dom_key = this.OVERVIEW_SECTION;
 
       //now populate the html page
       var str_html= "";
       var dom_key_arr = dom_key.split("-");
       for (var i = 0; i < dom_key_arr.length; i++) {
         var obj_dom_type = this.DOMTYPE[dom_key_arr[i]];
-        switch (obj_dom_type.type) {
-          case 'dropdown':
-                var str_options = "";
-                for (var j = 0; j < obj_dom_type.value.length; j++) {
-                  var opt_val = obj_dom_type.value[j];
-                  var opt_lbl = obj_dom_type.label[j];
-                  var selected_val = "";
-                  if (opt_val == node.value) {
-                    selected_val = "selected";
-                  }
-                  str_options = "<option value='"+opt_val+"' "+selected_val+">"+opt_lbl+"</option>"+str_options;
+        str_html = str_html + this.__build_corresponding_dom(obj_dom_type, this.GENERAL);
+      }
+      this.overview_section_html = str_html;
+    }
 
-                };
-                str_html = str_html + `
-                <div class="input-group">
-                      <div class="input-group-prepend">
-                        <label class="input-group-text">`+obj_dom_type.title+`</label>
-                      </div>
-                      <select class="val-box custom-select">`+str_options+`</select>
-                </div>
-                `;
-          break;
-          case 'input_box':
-            str_html = str_html + `
-            <div class="input-group">
-              <div class="input-group-prepend">
-                <label class="input-group-text">`+obj_dom_type.title+`</label>
-              </div>
-              <input class="val-box" value="`+node.name+`" type="text">
-            </div>
-            `;
-          break;
-        }
+    build_info(node) {
+      //first decide what doms should be visualized (defined in DOMTYPE)
+      var dom_key = this.INFO_SECTION[node.type];
+
+      //now populate the html page
+      var str_html= "";
+      var dom_key_arr = dom_key.split("-");
+      for (var i = 0; i < dom_key_arr.length; i++) {
+        var obj_dom_type = this.DOMTYPE[dom_key_arr[i]];
+        str_html = str_html + this.__build_corresponding_dom(obj_dom_type, node);
       }
 
       this.info_section_html = str_html;
+    }
+
+    __build_corresponding_dom(obj_dom_type, node){
+      var str_html= "";
+      switch (obj_dom_type.type) {
+        case 'dropdown':
+              var str_options = "";
+              for (var j = 0; j < obj_dom_type.value.length; j++) {
+                var opt_val = obj_dom_type.value[j];
+                var opt_lbl = obj_dom_type.label[j];
+                var selected_val = "";
+                if (opt_val == node.value) {
+                  selected_val = "selected";
+                }
+                str_options = "<option value='"+opt_val+"' "+selected_val+">"+opt_lbl+"</option>"+str_options;
+
+              };
+              str_html = str_html + `
+              <div class="input-group">
+                    <div class="input-group-prepend">
+                      <label class="input-group-text">`+obj_dom_type.title+`</label>
+                    </div>
+                    <select class="val-box custom-select">`+str_options+`</select>
+              </div>
+              `;
+        break;
+        case 'input_box':
+          str_html = str_html + `
+          <div class="input-group">
+            <div class="input-group-prepend">
+              <label class="input-group-text">`+obj_dom_type.title+`</label>
+            </div>
+            <input class="val-box" value="`+node[obj_dom_type.value]+`" type="text">
+          </div>
+          `;
+        break;
+      }
+
+      return str_html;
     }
 
     click_on_node(node){
